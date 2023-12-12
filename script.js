697ce8d158959e55e75d4259c30cd25545891db1
Jonathan Schelling <jojo.schelling@gmail.com>
2023-12-12 21:06:43 +0100

    added transitions, and trailer sidebar

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ function toggleVisibility(imageId, textId) {
 
 
 
+function delay (URL) {
+    var myDiv = document.getElementById('content'); 
 
+    
+    myDiv.style.transition = 'opacity 0.5s ease'; 
 
+    
+    myDiv.style.opacity = '0'; 
+
+    var myDiv2 = document.getElementsByClassName('socials')[0]; 
+
+    
+    myDiv2.style.transition = 'opacity 0.5s ease'; 
+
+    
+    myDiv2.style.opacity = '0'; 
+
+    setTimeout( function() { window.location = URL }, 500 );
+}
 
