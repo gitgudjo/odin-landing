da8107249bddf8253592d2560d01da2219cd9637
Jonathan Schelling <jojo.schelling@gmail.com>
2023-12-20 10:52:44 +0100

    added "browser" for photos and some icons

--- script.js
+++ script.js
@@ -38,3 +38,121 @@ function openFileExplorer(folderName) {
     document.getElementById('fileExplorer').style.display = 'none';
   }
   
+
+  function openBrowser(folderName) {
+    // You can customize this function to load content based on the folderName
+    // For simplicity, just toggle the visibility of the file explorer window
+    document.getElementById('browser').style.display = 'block';
+
+    if (folderName === 'Japanfolder') {
+        loadJapanContent();
+
+    } else if (folderName === 'albaniaFolder') {
+        loadAlbaniaContent()
+        
+    }
+
+    function loadJapanContent() {
+        
+        document.querySelector('#browser .title p').innerText = 'C:/Photos/Japan, 2019';
+    
+       
+        const photoroll = document.querySelector('#browser .photoroll');
+        photoroll.innerHTML = `
+        <img src="/photos/japan/IMG_9313.jpg" alt="">
+        <img src="/photos/japan/IMG_9301.jpg" alt="">
+        <img src="/photos/japan/IMG_0063.jpg" alt="">
+        <img src="/photos/japan/IMG_0048.jpg" alt="">
+        <div class="portrait">
+        <img src="/photos/japan/IMG_9534.jpg">
+    </div>
+        <img src="/photos/japan/IMG_9536.jpg" alt="">
+        <img src="/photos/japan/IMG_0047.jpg" alt="">
+        <div class="smaller">
+        <img src="/photos/japan/IMG_20190722_174509.jpg" alt="img">
+    </div>
+        <img src="/photos/japan/IMG_9316.jpg" alt="">
+        <div class="portrait">
+        <img src="/photos/japan/IMG_9345.jpg" alt="">
+    </div>
+        <img src="/photos/japan/IMG_9518.jpg" alt="">
+        <div class="portrait">
+        <img src="/photos/japan/IMG_9514.jpg" alt="">
+    </div>
+        `;
+    } 
+
+    function loadAlbaniaContent() {
+        document.querySelector('#browser .title p').innerText = 'C:/Photos/Albania, 2018';
+    
+       
+        const photoroll = document.querySelector('#browser .photoroll');
+        photoroll.innerHTML = `
+        <img src="/photos/albania/IMG_2047.jpg" alt="">
+               <img src="/photos/albania/IMG_2027.jpg" alt="">
+               <img src="/photos/albania/IMG_2039.jpg" alt="">
+               <img src="/photos/albania/IMG_2138.jpg" alt="">
+               <img src="/photos/albania/IMG_2143.jpg" alt="">
+               <img src="/photos/albania/IMG_2063.jpg" alt="">
+               <img src="/photos/albania/IMG_2020.jpg" alt="">
+               <img src="/photos/albania/IMG_2023.jpg" alt="">
+        `;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+  
+  function closeBrowser() {
+    document.getElementById('browser').style.display = 'none';
+    photorollElement.scrollTop = 0;
+  }
+
+  
